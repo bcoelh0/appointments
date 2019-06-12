151cc6858fb6447051efa8dcf6448c58c76a3f49
Bruno Coelho <brunocsncoelho@gmail.com>
2019-06-12 19:15:48 +0100

    adds alert boxes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,14 @@ module ApplicationHelper
       link_to link_text, link_path, class: "nav-link"
     end
   end
+
+  def alert_box(type, notice)
+    return unless notice.present?
+    content_tag(:div, class: "alert alert-#{type} alert-dismissible fade show", role: "alert") do
+      content_tag(:p, notice, style: "margin-bottom: 0") +
+      content_tag(:button, class: "close", "aria-label" => "Close", "data-dismiss" => "alert", :type => "button") do
+        content_tag(:span, " × ", "aria-hidden" => "true")
+      end
+    end
+  end
 end
